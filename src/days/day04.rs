7f154639ebf431df596fc2d25b05563ada5df8a9
Neil Henning <neil.henning@hey.com>
2022-12-04 09:02:51 +0000

    Day 4.

--- src/days/day04.rs
+++ src/days/day04.rs
@@ -1,11 +1,72 @@
 use crate::{Solution, SolutionPair};
 
+static INPUT: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/input/day04/input"));
+
 ///////////////////////////////////////////////////////////////////////////////
 
 pub fn solve() -> SolutionPair {
-    // Your solution here...
-    let sol1: u64 = 0;
-    let sol2: u64 = 0;
+    solver(INPUT)
+}
 
-    (Solution::U64(sol1), Solution::U64(sol2))
+fn solver(data: &str) -> SolutionPair {
+    let mut p1_overlaps = 0;
+    let mut p2_overlaps = 0;
+
+    for line in data.lines() {
+        let line = line.trim();
+
+        if line.is_empty() {
+            continue;
+        }
+
+        let mut splits = line
+            .split(|c| c == '-' || c == ',')
+            .map(|s| s.parse::<u32>().unwrap());
+
+        let p0 = splits.next().unwrap()..=splits.next().unwrap();
+        let p1 = splits.next().unwrap()..=splits.next().unwrap();
+
+        if (p0.contains(p1.start()) && p0.contains(p1.end()))
+            || (p1.contains(p0.start()) && p1.contains(p0.end()))
+        {
+            p1_overlaps += 1;
+        }
+
+        if p0.contains(p1.start())
+            || p0.contains(p1.end())
+            || p1.contains(p0.start())
+            || p1.contains(p0.end())
+        {
+            p2_overlaps += 1;
+        }
+    }
+
+    (Solution::U32(p1_overlaps), Solution::U32(p2_overlaps))
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::etc::Solution;
+
+    use super::solver;
+
+    static EXAMPLE: &str = r#"2-4,6-8
+    2-3,4-5
+    5-7,7-9
+    2-8,3-7
+    6-6,4-6
+    2-6,4-8
+"#;
+
+    #[test]
+    fn it_works() {
+        let solution = solver(EXAMPLE);
+
+        if let (Solution::U32(p1), Solution::U32(p2)) = solution {
+            assert_eq!(p1, 2);
+            assert_eq!(p2, 4);
+        } else {
+            panic!();
+        }
+    }
 }
