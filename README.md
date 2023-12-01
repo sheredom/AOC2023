90d20a3db4d38a64b2aebc76fc422ad8cb300440
Neil Henning <neil.henning@hey.com>
2023-12-01 09:06:26 +0000

    Make it 2023.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-🎄🦀🎄 Advent of Code 2022 Solutions 🎄🦀🎄
+🎄🦀🎄 Advent of Code 2023 Solutions 🎄🦀🎄
 
-Here is my Rust-based Advent of Code 2022 solutions.
+Here is my Rust-based Advent of Code 2023 solutions.
 
 ### Advent of Code project template
 A Rust template for Advent of Code that I made to easily run any day or combination of days and measure the execution time.
@@ -8,3 +8,5 @@ A Rust template for Advent of Code that I made to easily run any day or combinat
 Each day has a `solve()` function that returns a pair of `Solution`. The type `Solution` is an enum that can contain any integer or a string.
 
 To run: `cargo run --release [days...]`
+
+Use: `static INPUT: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/input/dayXX/input"));` each day.
--- src/days/day06.rs
+++ src/days/day06.rs
@@ -1,88 +1,11 @@
 use crate::{Solution, SolutionPair};
 
-static INPUT: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/input/day06/input"));
-
 ///////////////////////////////////////////////////////////////////////////////
 
 pub fn solve() -> SolutionPair {
-    solver(INPUT)
-}
+    // Your solution here...
+    let sol1: u64 = 0;
+    let sol2: u64 = 0;
 
-fn solver(data: &str) -> SolutionPair {
-    (solution::<4>(data), solution::<14>(data))
-}
-
-fn solution<const COUNT: usize>(data: &str) -> Solution {
-    let mut chars = data.chars();
-
-    let mut cache: [char; COUNT] = ['\0'; COUNT];
-
-    for item in cache.iter_mut().take(COUNT - 1) {
-        *item = chars.next().unwrap();
-    }
-
-    let mut insert_pos = COUNT - 1;
-
-    let mut part1 = None;
-
-    for (index, char) in chars.enumerate() {
-        cache[insert_pos] = char;
-
-        insert_pos += 1;
-        insert_pos %= COUNT;
-
-        // now check if the cache has anything in it that is a duplicate
-        let mut duplicate = false;
-
-        for (i, c0) in cache.iter().enumerate() {
-            for c1 in cache.iter().skip(i + 1) {
-                if c0 == c1 {
-                    duplicate = true;
-                    break;
-                }
-            }
-
-            if duplicate {
-                break;
-            }
-        }
-
-        if duplicate {
-            continue;
-        }
-
-        part1 = Some(COUNT + index);
-        break;
-    }
-
-    Solution::U32(part1.unwrap() as u32)
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::etc::Solution;
-
-    use super::solver;
-
-    static EXAMPLES: [(&str, u32); 5] = [
-        ("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7),
-        ("bvwbjplbgvbhsrlpgdmjqwftvncz", 5),
-        ("nppdvjthqldpwncqszvftbrmjlhg", 6),
-        ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10),
-        ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11),
-    ];
-
-    #[test]
-    fn it_works() {
-        for (example, r1) in EXAMPLES {
-            let solution = solver(example);
-
-            if let (Solution::U32(p1), Solution::U32(_)) = solution {
-                assert_eq!(p1, r1);
-                //assert_eq!(p2, "MCD");
-            } else {
-                panic!();
-            }
-        }
-    }
+    (Solution::U64(sol1), Solution::U64(sol2))
 }
