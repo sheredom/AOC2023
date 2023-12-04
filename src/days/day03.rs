93143aac62231fd504310c6302cbd470a9a969fe
Neil Henning <neil.henning@hey.com>
2023-12-04 13:23:30 +0000

    Day 4 solutions.

--- src/days/day03.rs
+++ src/days/day03.rs
@@ -1,11 +1,239 @@
+use std::collections::HashMap;
+
 use crate::{Solution, SolutionPair};
 
 ///////////////////////////////////////////////////////////////////////////////
 
-pub fn solve() -> SolutionPair {
-    // Your solution here...
-    let sol1: u64 = 0;
-    let sol2: u64 = 0;
+pub fn solve1(input: &str) -> Solution {
+    let mut sol = 0;
 
-    (Solution::U64(sol1), Solution::U64(sol2))
+    let mut len = None;
+
+    let mut board = Vec::new();
+
+    for line in input.lines() {
+        if let Some(len) = len {
+            assert_eq!(len, line.len() + 2);
+        } else {
+            // +2 for the edges!
+            len = Some(line.len() + 2);
+
+            // Before the first row we spam in a lot of empty tiles ('.').
+            board.resize(len.unwrap(), '.');
+        }
+
+        // Push a left hand boundary.
+        board.push('.');
+
+        for c in line.chars() {
+            board.push(c);
+        }
+
+        // Push a right hand boundary.
+        board.push('.');
+    }
+
+    let len = len.unwrap();
+
+    // After the last row we spam in a lot of empty tiles ('.').
+    board.resize(board.len() + len, '.');
+
+    // Only loop over the non-boundary tiles.
+    for y in 1..(len - 1) {
+        for x in 1..(len - 1) {
+            let index = y * len + x;
+
+            let tile = board[index];
+
+            // We are only looking for actual symbols here.
+            if tile.is_ascii_digit() || '.' == tile {
+                continue;
+            }
+
+            for other_index in [
+                index - len - 1,
+                index - len,
+                index - len + 1,
+                index - 1,
+                index + 1,
+                index + len - 1,
+                index + len,
+                index + len + 1,
+            ] {
+                let other = board[other_index];
+
+                // Only looking for digits here.
+                if !other.is_ascii_digit() {
+                    continue;
+                }
+
+                // We need to go left on the board until we hit a non digit.
+                let mut start_index = other_index;
+
+                while board[start_index].is_ascii_digit() {
+                    start_index -= 1;
+                }
+
+                // We'll be one behind the actual start, so bump now (and un-mut ourselves).
+                let start_index = start_index + 1;
+
+                let mut end_index = other_index;
+
+                while board[end_index].is_ascii_digit() {
+                    end_index += 1;
+                }
+
+                let mut part_number = 0;
+
+                for tile in board.iter_mut().take(end_index).skip(start_index) {
+                    part_number *= 10;
+                    part_number += tile.to_digit(10).unwrap();
+
+                    // Wipe out the part number so that we don't double count!
+                    *tile = '.';
+                }
+
+                sol += part_number;
+            }
+        }
+    }
+
+    Solution::U32(sol)
+}
+
+pub fn solve2(input: &str) -> Solution {
+    let mut len = None;
+
+    let mut board = Vec::new();
+
+    let mut gears = HashMap::new();
+
+    for line in input.lines() {
+        if let Some(len) = len {
+            assert_eq!(len, line.len() + 2);
+        } else {
+            // +2 for the edges!
+            len = Some(line.len() + 2);
+
+            // Before the first row we spam in a lot of empty tiles ('.').
+            board.resize(len.unwrap(), '.');
+        }
+
+        // Push a left hand boundary.
+        board.push('.');
+
+        for c in line.chars() {
+            board.push(c);
+        }
+
+        // Push a right hand boundary.
+        board.push('.');
+    }
+
+    let len = len.unwrap();
+
+    // After the last row we spam in a lot of empty tiles ('.').
+    board.resize(board.len() + len, '.');
+
+    // Only loop over the non-boundary tiles.
+    for y in 1..(len - 1) {
+        for x in 1..(len - 1) {
+            let index = y * len + x;
+
+            let tile = board[index];
+
+            // We are only looking for gears.
+            if '*' != tile {
+                continue;
+            }
+
+            for other_index in [
+                index - len - 1,
+                index - len,
+                index - len + 1,
+                index - 1,
+                index + 1,
+                index + len - 1,
+                index + len,
+                index + len + 1,
+            ] {
+                let other = board[other_index];
+
+                // Only looking for digits here.
+                if !other.is_ascii_digit() {
+                    continue;
+                }
+
+                // We need to go left on the board until we hit a non digit.
+                let mut start_index = other_index;
+
+                while board[start_index].is_ascii_digit() {
+                    start_index -= 1;
+                }
+
+                // We'll be one behind the actual start, so bump now (and un-mut ourselves).
+                let start_index = start_index + 1;
+
+                let mut end_index = other_index;
+
+                while board[end_index].is_ascii_digit() {
+                    end_index += 1;
+                }
+
+                let mut part_number = 0;
+
+                for tile in board.iter_mut().take(end_index).skip(start_index) {
+                    part_number *= 10;
+                    part_number += tile.to_digit(10).unwrap();
+
+                    // Wipe out the part number so that we don't double count!
+                    *tile = '.';
+                }
+
+                gears.entry(index).or_insert_with(Vec::new);
+
+                gears.get_mut(&index).unwrap().push(part_number);
+            }
+        }
+    }
+
+    let mut sol = 0;
+
+    for (_, gear) in gears {
+        if 2 != gear.len() {
+            continue;
+        }
+
+        sol += gear.iter().product::<u32>();
+    }
+
+    Solution::U32(sol)
+}
+
+pub fn solve() -> SolutionPair {
+    let input = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/input/day03"));
+
+    (solve1(input), solve2(input))
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    #[test]
+    fn it_works() {
+        let input = r"467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..";
+
+        assert_eq!(super::solve1(input), Solution::U32(4361));
+        assert_eq!(super::solve2(input), Solution::U32(467835));
+    }
 }
--- src/days/day04.rs
+++ src/days/day04.rs
@@ -1,11 +1,128 @@
+use std::collections::{HashMap, HashSet};
+
 use crate::{Solution, SolutionPair};
 
 ///////////////////////////////////////////////////////////////////////////////
 
-pub fn solve() -> SolutionPair {
-    // Your solution here...
-    let sol1: u64 = 0;
-    let sol2: u64 = 0;
+pub fn solve1(input: &str) -> Solution {
+    let mut sol = 0;
 
-    (Solution::U64(sol1), Solution::U64(sol2))
+    for line in input.lines() {
+        let mut winners = HashSet::new();
+
+        let (_, line) = line.split_once(':').unwrap();
+        let (winning_numbers, my_numbers) = line.split_once('|').unwrap();
+
+        for winning_number in winning_numbers.trim().split(' ') {
+            if winning_number.is_empty() {
+                continue;
+            }
+
+            let winning_number = winning_number.parse::<u8>().unwrap();
+            winners.insert(winning_number);
+        }
+
+        let mut number_found = 0;
+
+        for my_number in my_numbers.trim().split(' ') {
+            if my_number.is_empty() {
+                continue;
+            }
+
+            let my_number = my_number.parse::<u8>().unwrap();
+            if winners.contains(&my_number) {
+                number_found += 1;
+            }
+        }
+
+        // 0 -> 0, 1 -> 1, 2 -> 2, 3 -> 4, 4 -> 8, etc.
+        let points = if 0 == number_found {
+            0
+        } else {
+            2_u32.pow(number_found - 1)
+        };
+
+        sol += points;
+    }
+
+    Solution::U32(sol)
+}
+
+pub fn solve2(input: &str) -> Solution {
+    let mut scratch_card_hits = HashMap::new();
+
+    for (scratch_card_id, line) in input.lines().enumerate() {
+        let mut winners = HashSet::new();
+
+        let (_, line) = line.split_once(':').unwrap();
+        let (winning_numbers, my_numbers) = line.split_once('|').unwrap();
+
+        for winning_number in winning_numbers.trim().split(' ') {
+            if winning_number.is_empty() {
+                continue;
+            }
+
+            let winning_number = winning_number.parse::<u8>().unwrap();
+            winners.insert(winning_number);
+        }
+
+        let mut number_found = 0;
+
+        for my_number in my_numbers.trim().split(' ') {
+            if my_number.is_empty() {
+                continue;
+            }
+
+            let my_number = my_number.parse::<u8>().unwrap();
+
+            if winners.contains(&my_number) {
+                number_found += 1;
+            }
+        }
+
+        scratch_card_hits.entry(scratch_card_id).or_insert(0);
+
+        *scratch_card_hits.get_mut(&scratch_card_id).unwrap() += 1;
+
+        if 0 == number_found {
+            continue;
+        }
+
+        for index in 1..=number_found {
+            let id = scratch_card_id + index;
+
+            scratch_card_hits.entry(id).or_insert(0);
+
+            *scratch_card_hits.get_mut(&id).unwrap() +=
+                *scratch_card_hits.get(&scratch_card_id).unwrap();
+        }
+    }
+
+    let sol = scratch_card_hits.iter().fold(0, |acc, (_, v)| acc + *v);
+
+    Solution::U32(sol)
+}
+
+pub fn solve() -> SolutionPair {
+    let input = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/input/day04"));
+
+    (solve1(input), solve2(input))
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    #[test]
+    fn it_works() {
+        let input = r"Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+
+        assert_eq!(super::solve1(input), Solution::U32(13));
+        assert_eq!(super::solve2(input), Solution::U32(30));
+    }
 }
