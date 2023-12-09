f45b8e0fee3ee3617afa979450b1a0db4a384c20
Neil Henning <neil.henning@hey.com>
2023-12-09 09:00:00 +0000

    Day 8 and 9 solutions.

--- src/days/day08.rs
+++ src/days/day08.rs
@@ -1,11 +1,157 @@
+use std::collections::HashMap;
+
 use crate::{Solution, SolutionPair};
 
 ///////////////////////////////////////////////////////////////////////////////
 
-pub fn solve() -> SolutionPair {
-    // Your solution here...
-    let sol1: u64 = 0;
-    let sol2: u64 = 0;
+pub fn solve1(input: &str) -> Solution {
+    let instructions = input.lines().next().unwrap();
 
-    (Solution::U64(sol1), Solution::U64(sol2))
+    let mut map = HashMap::new();
+
+    for line in input.lines().skip(1) {
+        if line.is_empty() {
+            continue;
+        }
+
+        let (node, line) = line.split_once('=').unwrap();
+        let node = node.trim();
+
+        let (left, right) = line.split_once(',').unwrap();
+        let left = left.trim_matches(|x| matches!(x, '(' | ')' | ',' | ' '));
+        let right = right.trim_matches(|x| matches!(x, '(' | ')' | ',' | ' '));
+
+        assert_eq!(3, node.len());
+        assert_eq!(3, left.len());
+        assert_eq!(3, right.len());
+
+        map.insert(node, (left, right));
+    }
+
+    let mut current = "AAA";
+    let mut index = 0;
+    let mut steps = 0;
+
+    while "ZZZ" != current {
+        let instruction = instructions.chars().nth(index).unwrap();
+
+        match instruction {
+            'L' => current = map[current].0,
+            'R' => current = map[current].1,
+            _ => unreachable!(),
+        }
+
+        index += 1;
+        index %= instructions.len();
+
+        steps += 1;
+    }
+
+    Solution::U32(steps)
+}
+
+pub fn solve2(input: &str) -> Solution {
+    let instructions = input.lines().next().unwrap();
+
+    let mut current = Vec::new();
+    let mut map = HashMap::new();
+
+    for line in input.lines().skip(1) {
+        if line.is_empty() {
+            continue;
+        }
+
+        let (node, line) = line.split_once('=').unwrap();
+        let node = node.trim();
+
+        if node.ends_with('A') {
+            current.push(node);
+        }
+
+        let (left, right) = line.split_once(',').unwrap();
+        let left = left.trim_matches(|x| matches!(x, '(' | ')' | ',' | ' '));
+        let right = right.trim_matches(|x| matches!(x, '(' | ')' | ',' | ' '));
+
+        assert_eq!(3, node.len());
+        assert_eq!(3, left.len());
+        assert_eq!(3, right.len());
+
+        map.insert(node, (left, right));
+    }
+
+    let sol = current
+        .iter()
+        .map(|x| {
+            let mut x = *x;
+
+            let mut index = 0;
+            let mut steps = 0;
+
+            while !x.ends_with('Z') {
+                let instruction = instructions.chars().nth(index).unwrap();
+
+                match instruction {
+                    'L' => x = map[x].0,
+                    'R' => x = map[x].1,
+                    _ => unreachable!(),
+                }
+
+                index += 1;
+                index %= instructions.len();
+
+                steps += 1;
+            }
+
+            steps as u64
+        })
+        .fold(1, num::integer::lcm);
+
+    Solution::U64(sol)
+}
+
+pub fn solve() -> SolutionPair {
+    let input = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/input/day08"));
+
+    (solve1(input), solve2(input))
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    #[test]
+    fn it_works() {
+        let input = r"RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)";
+
+        assert_eq!(super::solve1(input), Solution::U32(2));
+
+        let input = r"LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)";
+
+        assert_eq!(super::solve1(input), Solution::U32(6));
+
+        let input = r"LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)";
+
+        assert_eq!(super::solve2(input), Solution::U64(6));
+    }
 }
--- src/days/day09.rs
+++ src/days/day09.rs
@@ -1,11 +1,101 @@
+use itertools::*;
+use num::Zero;
+
 use crate::{Solution, SolutionPair};
 
 ///////////////////////////////////////////////////////////////////////////////
 
-pub fn solve() -> SolutionPair {
-    // Your solution here...
-    let sol1: u64 = 0;
-    let sol2: u64 = 0;
+pub fn solve1(input: &str) -> Solution {
+    let mut sol = 0;
 
-    (Solution::U64(sol1), Solution::U64(sol2))
+    for line in input.lines() {
+        let mut sequences = Vec::new();
+
+        sequences.push(Vec::new());
+
+        for element in line.split(' ') {
+            let element = element.parse::<i32>().unwrap();
+            sequences[0].push(element);
+        }
+
+        while !sequences.last().unwrap().iter().all(i32::is_zero) {
+            let mut sequence = Vec::new();
+
+            for (e, n) in sequences.last().unwrap().iter().tuple_windows() {
+                sequence.push(n - e);
+            }
+
+            sequences.push(sequence);
+        }
+
+        let mut diff = 0;
+
+        for sequence in sequences.iter().rev() {
+            let last = sequence.last().unwrap();
+
+            diff += last;
+        }
+
+        sol += diff;
+    }
+
+    Solution::I32(sol)
+}
+
+pub fn solve2(input: &str) -> Solution {
+    let mut sol = 0;
+
+    for line in input.lines() {
+        let mut sequences = Vec::new();
+
+        sequences.push(Vec::new());
+
+        for element in line.split(' ') {
+            let element = element.parse::<i32>().unwrap();
+            sequences[0].push(element);
+        }
+
+        while !sequences.last().unwrap().iter().all(i32::is_zero) {
+            let mut sequence = Vec::new();
+
+            for (e, n) in sequences.last().unwrap().iter().tuple_windows() {
+                sequence.push(n - e);
+            }
+
+            sequences.push(sequence);
+        }
+
+        let mut diff = 0;
+
+        for sequence in sequences.iter().rev() {
+            let first = sequence.first().unwrap();
+
+            diff = first - diff;
+        }
+
+        sol += diff;
+    }
+
+    Solution::I32(sol)
+}
+
+pub fn solve() -> SolutionPair {
+    let input = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/input/day09"));
+
+    (solve1(input), solve2(input))
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    #[test]
+    fn it_works() {
+        let input = r"0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45";
+
+        assert_eq!(super::solve1(input), Solution::I32(114));
+        assert_eq!(super::solve2(input), Solution::I32(2));
+    }
 }
